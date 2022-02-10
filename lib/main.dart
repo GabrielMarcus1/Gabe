79bee4d7daf7c4a37a868628236a8cb10535fa2a
Gabe Marcus <mrcgab004@myuct.ac.za>
2022-02-10 12:16:50 +0200

    v1.4 second page when lick a line is live

--- lib/main.dart
+++ lib/main.dart
@@ -2,13 +2,19 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_2/PaymentHistoryInformation.dart';
 
+import 'SecondPage.dart';
+
+
 void main() async{
- final response= ({"contractNumber":"AC499702","type":"MM","amount":38000,"date":"2018-05-04T00:00:00.000Z","mobile":"265995935569","status":"ACCEPTED","ccy":"MWK","transactionId":"MP180504.0841.G38071"});
-//print(response);
-PaymentHistoryInformation phi= PaymentHistoryInformation.fromJson((response));
+ final response= [{"contractNumber":"AC499702","type":"MM","amount":38000,"date":"2018-05-04T00:00:00.000Z","mobile":"265995935569","status":"ACCEPTED","ccy":"MWK","transactionId":"MP180504.0841.G38071"},
+ {"contractNumber":"AC499702","type":"MM","amount":39000,"date":"2018-05-04T00:00:00.000Z","mobile":"265995935569","status":"ACCEPTED","ccy":"MWK","transactionId":"MP180504.0841.G38071"}];
+PaymentHistoryInformation phi= PaymentHistoryInformation.fromJson((response[1]));
+phi.toJson(); //make a loop to populate toJson the responses 
+  var monz=phi.amount;
+  /////JSON IS ALREADY IN ARRAYS LOOK AT PAYMENT HISTORYINFORMATION
   runApp(
     MyApp(
-      items: List<String>.generate(10, (i) => phi.amount.toString()),
+      items: List<String>.generate(2, (i) => phi.amount.toString()),
     ),
   );
 }
@@ -20,7 +26,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    const title = 'Long List';
+    const title = 'Payment History';
 
     return MaterialApp(
       title: title,
@@ -33,10 +39,48 @@ class MyApp extends StatelessWidget {
           itemBuilder: (context, index) {
             return ListTile(
               title: Text(items[index]),
+         onTap: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const SecondRoute()),
             );
           },
+          );
+          
+          }
+          
+        )
+        
+        ));
+       
+      
+      }
+}
+
+/**
+ * Second page which will displayfurther details
+ */
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text('Go back!'),
         ),
       ),
     );
   }
 }
+
+
+            
+
